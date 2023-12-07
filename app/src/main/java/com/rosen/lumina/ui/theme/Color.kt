f5c8aff11935b98dc835f7b0608a39f7e4c2fbdc
DerekWasswa <wasswadero@gmail.com>
2023-12-07 17:06:21 +0300

    initial project set up and app theme

---
+++ app/src/main/java/com/rosen/lumina/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.rosen.lumina.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val LightGray = Color(0xFFF4F4F4)
+val PurpleBlue = Color(0xFF4D4672)
+val LighterGray = Color(0xFFEEEEEE)
+val OrangeBrown = Color(0xFFFA6B1A)
+val Peach = Color(0xFFF4BB7E)
+val White = Color(0xFFFFFFFF)
+val CharcoalBlack = Color(0xFF090909)
+val DarkShadeBlack = Color(0xFF030303)
+val DarkGray = Color(0xFF111111)
---
+++ app/src/main/java/com/rosen/lumina/ui/theme/Type.kt
@@ -0,0 +1,41 @@
+package com.rosen.lumina.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.rosen.lumina.R
+
+val Exo = FontFamily(
+    Font(R.font.exo_regular, FontWeight.Normal),
+    Font(R.font.exo_medium, FontWeight.Medium),
+    Font(R.font.exo_light, FontWeight.Light),
+    Font(R.font.exo_bold, FontWeight.Bold),
+    Font(R.font.exo_semi_bold, FontWeight.SemiBold)
+)
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = Exo,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = Exo,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = Exo,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+)
